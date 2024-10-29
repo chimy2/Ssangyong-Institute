4194369ddadeaedd9f573db00968578368922db3
chimy2 <premet@naver.com>
2024-10-29 19:54:52 +0900

    2024년 10월 29일
    Spring REST Api Document

---
+++ spring/AjaxServer/src/main/java/com/test/ajax/dto/UserDTO.java
@@ -0,0 +1,15 @@
+package com.test.ajax.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+	
+	private String id;
+	private String name;
+	private String gender;
+}
